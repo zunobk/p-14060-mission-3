47b0d9c45e8b16d3444771e650fc436b842bcd6a
zunobk <junho4560@naver.com>
2025-11-11 12:41:25 +0900

    스텝 02, green

--- src/test/java/com/back/standard/util/TestUtil.java
+++ src/test/java/com/back/standard/util/TestUtil.java
@@ -1,9 +1,28 @@
 package com.back.standard.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
+    private static final PrintStream ORIGINAL_OUT = System.out;
+    private static PrintStream CURRENT_OUT;
+
     public static Scanner genScanner(String input) {
         return new Scanner(input);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        CURRENT_OUT = new PrintStream(byteArrayOutputStream, true);
+        System.setOut(CURRENT_OUT);
+
+        return byteArrayOutputStream;
+    }
+
+    public static void clearSetOutToByteArray() {
+        System.setOut(ORIGINAL_OUT);
+        CURRENT_OUT.close();
+        CURRENT_OUT = null;
     }
 }
