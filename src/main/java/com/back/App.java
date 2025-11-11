9739c5bd03f15c734eeb0a9c685284d6591b2c4d
zunobk <junho4560@naver.com>
2025-11-11 12:45:09 +0900

    스텝 03, green

---
+++ src/main/java/com/back/App.java
@@ -0,0 +1,15 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+    }
+}
