5c8af14362bdc1a5860eb0d8661d927619904e57
zunobk <junho4560@naver.com>
2025-11-11 12:31:52 +0900

    스텝 01, green

---
+++ src/test/java/com/back/standard/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.back.standard.util;
+
+import java.util.Scanner;
+
+public class TestUtil {
+    public static Scanner genScanner(String input) {
+        return new Scanner(input);
+    }
+}
