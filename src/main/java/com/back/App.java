60ae096edee728b656d891963161777b896157df
zunobk <junho4560@naver.com>
2025-11-11 14:21:41 +0900

    커밋 - 스텝 05, green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -23,6 +23,8 @@ public class App {
 
                     System.out.print("작가 : ");
                     String author = scanner.nextLine();
+
+                    System.out.println("1번 명언이 등록되었습니다.");
                 }
                 case "종료" -> {
                     return;
