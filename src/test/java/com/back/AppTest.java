2040d60b39df492bbe60c83417064c11cf29d23e
zunobk <junho4560@naver.com>
2025-11-11 14:35:54 +0900

    스텝 07, red

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -59,4 +59,24 @@ public class AppTest {
                 .contains("1번 명언이 등록되었습니다.")
                 .contains("2번 명언이 등록되었습니다.");
     }
+
+    @Test
+    @DisplayName("목록")
+    void t5() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                """);
+
+        assertThat(rs)
+                .contains("번호 / 작가 / 명언")
+                .contains("----------------------")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
