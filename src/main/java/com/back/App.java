232ef97e715a9339df1b326d8f9dd54de2965a72
zunobk <junho4560@naver.com>
2025-11-11 14:25:39 +0900

    스텝 06, green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -12,6 +12,8 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -24,7 +26,9 @@ public class App {
                     System.out.print("작가 : ");
                     String author = scanner.nextLine();
 
-                    System.out.println("1번 명언이 등록되었습니다.");
+                    int id = ++lastId;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                 }
                 case "종료" -> {
                     return;
