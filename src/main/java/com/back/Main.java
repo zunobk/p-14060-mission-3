b076a4bdea385d643a3e4749184174a81279e216
zunobk <junho4560@naver.com>
2025-11-11 12:28:27 +0900

    출력을 모니터가 아닌 별도의 문자열로 받을 수 있는지 실험

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,10 +1,13 @@
 package com.back;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        // lab1();
+        lab2();
     }
 
     private static void lab1() {
@@ -20,5 +23,29 @@ public class Main {
 
         System.out.println("cmd : " + cmd);
         System.out.println("content : " + content);
+    }
+
+    private static void lab2() {
+        System.out.println("안녕하세요.");
+
+        // 기존의 output 스트림 저장
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream, true);
+        System.setOut(printStream);
+
+        System.out.println("반갑습니다.");
+        String str1 = byteArrayOutputStream.toString();
+
+        System.out.println("좋아요. 하하");
+        String str2 = byteArrayOutputStream.toString();
+
+        // 기존의 output 스트림 복원
+        System.setOut(ORIGINAL_OUT);
+        printStream.close();
+
+        System.out.println("str1 : " + str1);
+        System.out.println("str2 : " + str2);
     }
 }
