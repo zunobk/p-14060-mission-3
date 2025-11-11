e930e36ac0d074c2fb068aef5254b30e5d1c536a
zunobk <junho4560@naver.com>
2025-11-11 12:48:21 +0900

    스텝 03, blue

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -1,11 +1,7 @@
 package com.back;
 
-import com.back.standard.util.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,11 +9,7 @@ public class AppTest {
     @Test
     @DisplayName("`== 명언 앱 ==` 출력")
     void t1() {
-        Scanner scanner = TestUtil.genScanner("종료");
-
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-        new App(scanner).run();
-        String rs = output.toString();
+        String rs = AppTestRunner.run("종료");
 
         assertThat(rs)
                 .contains("== 명언 앱 ==");
---
+++ src/test/java/com/back/AppTestRunner.java
@@ -0,0 +1,18 @@
+package com.back;
+
+import com.back.standard.util.TestUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+public class AppTestRunner {
+
+    public static String run(String input) {
+        Scanner scanner = TestUtil.genScanner(input);
+
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        new App(scanner).run();
+
+        return output.toString();
+    }
+}
