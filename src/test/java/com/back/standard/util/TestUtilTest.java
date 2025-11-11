a8d83d563904da310b2380b80d221a2abf21a114
zunobk <junho4560@naver.com>
2025-11-11 12:37:59 +0900

    스텝 02, red

--- src/test/java/com/back/standard/util/TestUtilTest.java
+++ src/test/java/com/back/standard/util/TestUtilTest.java
@@ -3,6 +3,7 @@ package com.back.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,5 +25,19 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
         assertThat(author).isEqualTo("이순신");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    public void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray();
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+        System.out.println("이제는 화면에 출력됩니다.");
     }
 }
