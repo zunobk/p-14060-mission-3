ae6aca32ea0c5b2bd1dbe0041d9fb9587f34882f
zunobk <junho4560@naver.com>
2025-11-11 12:02:54 +0900

    뉴라인으로 여러 문장을 입력

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,13 +8,14 @@ public class Main {
     }
 
     private static void lab1() {
-        String input = "등록";
+        String input = "등록\n나의 죽음을 적들에게 알리지 말라.";
 
         Scanner scanner = new Scanner(input);
 
         String cmd = scanner.nextLine();
         String content = scanner.nextLine();
 
-        System.out.println(cmd);
+        System.out.println("cmd : " + cmd);
+        System.out.println("content : " + content);
     }
 }
