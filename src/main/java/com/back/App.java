53c77e2e3455fa0918c0b02bc1ee55500f59e9cc
zunobk <junho4560@naver.com>
2025-11-11 14:39:04 +0900

    스텝 07, green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -13,6 +15,8 @@ public class App {
         System.out.println("== 명언 앱 ==");
 
         int lastId = 0;
+
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -28,7 +32,17 @@ public class App {
 
                     int id = ++lastId;
 
+                    wiseSayings.add(new WiseSaying(id, content, author));
+
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for (WiseSaying wiseSaying : wiseSayings.reversed()) {
+                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+                    }
                 }
                 case "종료" -> {
                     return;
---
+++ src/main/java/com/back/WiseSaying.java
@@ -0,0 +1,37 @@
+package com.back;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
